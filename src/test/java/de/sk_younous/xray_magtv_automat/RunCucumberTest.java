b91d5130362d6b0c5cb2b3dd35a4e73847083b1d
unknown <amnay2604@yahoo.de>
2024-10-09 17:06:41 +0200

    update

--- src/test/java/de/sk_younous/xray_magtv_automat/RunCucumberTest.java
+++ src/test/java/de/sk_younous/xray_magtv_automat/RunCucumberTest.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {"pretty",
                 "junit:target/cucumber-reports/Cucumber.xml",
                 "html:target/cucumber-reports/Cucumber.html",
+                "json:target/cucumber-reports/Cucumber.json",
                 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
         },
         monochrome = true
